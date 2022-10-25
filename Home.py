bef8a2a2460f9280ada34605773bcbbaa9016382
alexdahlpublic <116271275+alexdahlpublic@users.noreply.github.com>
2022-10-25 20:21:28 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -98,7 +98,7 @@ if selected == "Statistik":
 
 
     st.write(f"""Sveriges produktion av el består i dagsläget främst av vattenkraft, kärnkraft, vindkraft   
-    samt kraftvärme. I 2021 utgjorde solkraften en liten andel, mindre än 1 % av Sveriges elproduktion. Däremot producerade
+    samt värmekraft. År 2021 utgjorde solkraften en liten andel, mindre än 1 % av Sveriges elproduktion. Däremot producerade
     solkraften 1.1 TWh, en ökning på 40 procent jämfört med 2020. 
    
     """)
